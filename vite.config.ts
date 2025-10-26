8d7137d2fdc8c207753f68ad1831af78b69bf7cf
Vincent Winkler <vwinkler@posteo.de>
2025-10-26 18:57:11 +0100

    Handle requests to spotify with MSW in dev

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,41 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { setupServer } from 'msw/node'
+import { handlers } from './src/spotify/request_handlers.testutils'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    mswPlugin(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://accounts.spotify.com',
+        changeOrigin: true,
+      }
+    }
+  }
 })
+
+function mswPlugin() {
+  const server = setupServer(...handlers)
+
+  return {
+    name: 'msw-server',
+    configureServer() {
+      server.listen({ onUnhandledRequest: 'error' })
+    },
+    closeBundle() {
+      server.close()
+    }
+  }
+}
